c88a8f02f1c13f2fd6bed33d46e00276570364a2
Timothy Lowe <65925429+t111mll@users.noreply.github.com>
2025-11-10 00:28:48 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] grid = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j= 0; j < 3; j++)
+    {
+        Console.Write(j);
+    }
+    Console.WriteLine();
+}
